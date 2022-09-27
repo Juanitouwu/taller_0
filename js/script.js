e1e6e3f7b2ab68c93c029bb0cbd2e3c294c1cd3d
juanca0108 <109818872+Juanitouwu@users.noreply.github.com>
2022-09-27 12:09:08 -0500

    correcciones de grid

--- js/script.js
+++ js/script.js
@@ -4,10 +4,22 @@ document.getElementById("logo-1").addEventListener("click", open_close_menu);
 var side_menu = document.getElementById("menu_side");
 var btn_open = document.getElementById("logo-1");
 var body = document.getElementById("body");
+const img = document.getElementById('logo-1');
+var pr_open = document.getElementsByClassName("profile")
 
 
 function open_close_menu(){
-    body.classList.toggle("body_move");
-    side_menu.classList.toggle("menu_side_move");
+
+    if(side_menu.classList.toggle("menu_side_move")){
+        img.setAttribute("src","img/Hotelia horizontal blanco.svg");
+        img.style.width="130px";
+    }
+
+    else{
+        img.setAttribute("src","img/hotelia campana.svg");
+        img.style.width="40px";
+    }
+
+    body.classList.toggle("body_move"); 
 
 }
